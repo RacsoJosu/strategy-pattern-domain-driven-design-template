b0153e208f9e80a268716a80349ecc22f47a984f
Oscar Vallecillo <oscarvallecillo95@gmail.com>
2026-02-23 00:09:18 -0600

    refactor code

--- main.ts
+++ main.ts
@@ -1,15 +1,11 @@
 import { createRouter } from "./src/routes/index";
 import { createApp } from "./src/app/app";
 import express from "express";
-
-// 2. Crear rutas inyectando el controlador
 const server = express();
 const transactionRoutes = createRouter();
-
-// 3. Crear la aplicación Express con las rutas
 const app = createApp({ router: transactionRoutes, app: server });
-
-// 4. Iniciar el servidor
 app.listen(3001, () => {
   console.log(`Servidor escuchando en http://localhost:3001 en modo development`);
 });
+
+
--- src/api/excel/infraestructure/strategy-manager.ts
+++ src/api/excel/infraestructure/strategy-manager.ts
@@ -1,5 +1,7 @@
+import { ExcelStrategy } from "../domain/excel-strategy";
 import { StrategyA } from "../domain/strategy-a";
 import { StrategyB } from "../domain/strategy-b";
+import { StrategyFactory } from "./strategy-factory";
 
 const strategies = {
   strategyA: new StrategyA(),
@@ -8,6 +10,21 @@ const strategies = {
 
 export type StrategyKey = keyof typeof strategies;
 
-export const strategyManager = {
-  getStrategy: (key: StrategyKey) => strategies[key],
-};
+
+export class StrategyManager implements StrategyFactory {
+   private strategies = new Map<string, ExcelStrategy>();
+
+    register(key: string, strategy: ExcelStrategy): void {
+    this.strategies.set(key, strategy);
+  }
+
+
+  getStrategy(type: StrategyKey) {
+    const startegy = strategies[type];
+    if (!startegy) {
+      throw new Error("Strategy not found");
+    }
+    return startegy;
+  }
+}
+
